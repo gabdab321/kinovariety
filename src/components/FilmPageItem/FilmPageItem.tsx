16a6b876eadccd64018614dadcb6cf77c0339c96
Ivan Postryhan <vaniapostrigan106@gmail.com>
2022-09-01 11:25:57 +0300

    created responsive layout for film page

--- src/components/FilmPageItem/FilmPageItem.tsx
+++ src/components/FilmPageItem/FilmPageItem.tsx
@@ -16,19 +16,19 @@ const FilmPageItem = ({film}: FilmPageItemProps) => {
             <h1 className={cl.item__title}>{film.nameRu}</h1>
             <h3 className={cl.item__subtitle}>{film.nameOriginal || film.nameEn}</h3>
 
-            <div className={cl.item__posterContainer}>
+            <div className={cl.item__info_container}>
                 <img className={cl.item__poster} src={film.posterUrlPreview} alt=""/>
-            </div>
 
-            <ul className={cl.item__film_about}>
-                <li className={cl.item__rating}>Рейтинг: {film.ratingImdb}</li>
-                <li className={cl.item__slogan}>Слоган: «{film.slogan}»</li>
-                <li className={cl.item__year}>Рік виходу: {film.year}</li>
-                <li className={cl.item__countries}>Країни: {transformArrayToStr("country", film.countries)}</li>
-                <li className={cl.item__}>Жанри: {transformArrayToStr("genre", film.genres)}</li>
-                <li className={cl.item__}>Вікові обмеження: {getAgeLimit(film.ratingAgeLimits)}</li>
-                <li className={cl.item__}>Тривалість фільму: {film.filmLength} хвилини</li>
-            </ul>
+                <ul className={cl.item__film_about}>
+                    <li className={cl.item__about_item}><span>Рейтинг: </span> {film.ratingImdb}</li>
+                    <li className={cl.item__about_item}><span>Слоган: </span> «{film.slogan}»</li>
+                    <li className={cl.item__about_item}><span>Рік виходу: </span> {film.year}</li>
+                    <li className={cl.item__about_item}><span>Країни: </span> {transformArrayToStr("country", film.countries)}</li>
+                    <li className={cl.item__about_item}><span>Жанри: </span> {transformArrayToStr("genre", film.genres)}</li>
+                    <li className={cl.item__about_item}><span>Вікові обмеження: </span> {getAgeLimit(film.ratingAgeLimits)}</li>
+                    <li className={cl.item__about_item}><span>Тривалість фільму: </span> {film.filmLength} хвилин</li>
+                </ul>
+            </div>
 
             <h2 className={cl.item__description_title}>Опис фільму</h2>
             <p className={cl.item__description}>{film.description}</p>
