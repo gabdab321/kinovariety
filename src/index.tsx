551216f38c5a954c590bd91c5fc1cacb88273fac
Ivan Postryhan <vaniapostrigan106@gmail.com>
2023-05-02 14:24:49 +0300

    deploy

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-    <BrowserRouter>
+    <BrowserRouter basename="https://kinovariety.netlify.app/">
         <Provider store={store}>
             <App />
         </Provider>
